f3ca97fb95fda11c1c4f74f2308ea0a066f3e11f
Hiuzton <102302338+Hiuzton@users.noreply.github.com>
2025-02-13 14:17:03 +0200

    Update resumeData.ts

--- src/data/resumeData.ts
+++ src/data/resumeData.ts
@@ -22,9 +22,9 @@ export const educations = [
 export const workExperiences = [
   {
     company: "SC Continental",
-    position: "System Testing Tool Developer Intern",
+    position: "Tool Developer Intern",
     period: "2023-2025",
-    description: "Developed tool for uC(micro-controller) testing",
+    description: "Developed tool for uC(micro-controller) testing using C#, .NET Framework, WPF",
   },
 ];
 
@@ -33,20 +33,49 @@ export const projects = [
     id: 1,
     title: "PhotoHub",
     description: "Scalable backend system with microservices architecture",
-    technologies: ["ASP.NET Core", "MSSQL", "Razor", "Html/Css"],
+    technologies: ["C#", "ASP.NET Core", "MSSQL", "Razor", "Html/Css", "Bootstrap",  "Visual Studio"],
     image:
       "https://private-user-images.githubusercontent.com/102302338/411689729-b340f017-5242-4392-90f4-cb2fcebcaca1.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzkyMTIyMDcsIm5iZiI6MTczOTIxMTkwNywicGF0aCI6Ii8xMDIzMDIzMzgvNDExNjg5NzI5LWIzNDBmMDE3LTUyNDItNDM5Mi05MGY0LWNiMmZjZWJjYWNhMS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjEwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxMFQxODI1MDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yNjExMjhlMTY1ZjE4ZjEzZjJhMjhkYjc5ZjA2NmYxMWI4NzFiNGRhNzY1YmRmMTI3ZGRhOGNjYTJhYWU5MDFhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.i9WWS0m0QZ5qKipxIZqo4Uy71FWRp0RuPcHsUlEQzr0",
     link: "https://github.com/Hiuzton/PhotoHub",
   },
   {
     id: 2,
-    title: "Blaster Game",
-    description: "A retro game in 90's style.",
-    technologies: ["C#", "WinForms", "MultiThreading"],
+    title: "CV Web page",
+    description: "CV Web page, new POV to CV organization",
+    technologies: ["React (TS)", "Html/Css", "Bootstrap", "EmailJS", "VS Code"],
+    image:
+      "https://private-user-images.githubusercontent.com/102302338/411689729-b340f017-5242-4392-90f4-cb2fcebcaca1.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzkyMTIyMDcsIm5iZiI6MTczOTIxMTkwNywicGF0aCI6Ii8xMDIzMDIzMzgvNDExNjg5NzI5LWIzNDBmMDE3LTUyNDItNDM5Mi05MGY0LWNiMmZjZWJjYWNhMS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjEwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxMFQxODI1MDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yNjExMjhlMTY1ZjE4ZjEzZjJhMjhkYjc5ZjA2NmYxMWI4NzFiNGRhNzY1YmRmMTI3ZGRhOGNjYTJhYWU5MDFhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.i9WWS0m0QZ5qKipxIZqo4Uy71FWRp0RuPcHsUlEQzr0",
+    link: "https://github.com/Hiuzton/CV_frontend",
+  },
+  {
+    id: 3,
+    title: "BookStore API",
+    description: "Backend service for virtual book store",
+    technologies: ["C#", "ASP.NET Core", "PostgreSQL", "JWT Token", "React (TS)", "Html/Css", "Bootstrap", "Visual Studio"],
     image:
       "https://private-user-images.githubusercontent.com/102302338/411680646-08a845ed-29fe-4bc9-a770-2b80827298cc.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzkyMTA1MDQsIm5iZiI6MTczOTIxMDIwNCwicGF0aCI6Ii8xMDIzMDIzMzgvNDExNjgwNjQ2LTA4YTg0NWVkLTI5ZmUtNGJjOS1hNzcwLTJiODA4MjcyOThjYy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjEwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxMFQxNzU2NDRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yOGI5YTVkMzY0OTE4ZDBiOWFjYjY2MDY3NDc2YzY2NWNjZTc5MjBhNjQxNWFjZTcwYmIzYzM0ODY2YWY2N2I0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.n3YSEwHw-acphlX8a7fkpVVVpUVFyo82MVYDR83-jG0",
-    link: "https://github.com/Hiuzton/Retro-Game",
+    link: "https://github.com/Hiuzton/BookStore.API",
+  },
+  {
+    id: 4,
+    title: "CChat",
+    description: "CChat is a real-livetime chat",
+    technologies: ["Java", "Firebase Services", "XML design", "Android Studio"],
+    image:
+      "https://private-user-images.githubusercontent.com/102302338/411680646-08a845ed-29fe-4bc9-a770-2b80827298cc.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzkyMTA1MDQsIm5iZiI6MTczOTIxMDIwNCwicGF0aCI6Ii8xMDIzMDIzMzgvNDExNjgwNjQ2LTA4YTg0NWVkLTI5ZmUtNGJjOS1hNzcwLTJiODA4MjcyOThjYy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjEwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxMFQxNzU2NDRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yOGI5YTVkMzY0OTE4ZDBiOWFjYjY2MDY3NDc2YzY2NWNjZTc5MjBhNjQxNWFjZTcwYmIzYzM0ODY2YWY2N2I0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.n3YSEwHw-acphlX8a7fkpVVVpUVFyo82MVYDR83-jG0",
+    link: "https://github.com/Hiuzton/CChat",
+  },
+  {
+    id: 5,
+    title: "E-Courses",
+    description: "Platform for virtual courses",
+    technologies: ["PHP", "Html/Css", "Javascript", "JQuery", "SMTP Email Service"],
+    image:
+      "https://private-user-images.githubusercontent.com/102302338/411680646-08a845ed-29fe-4bc9-a770-2b80827298cc.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzkyMTA1MDQsIm5iZiI6MTczOTIxMDIwNCwicGF0aCI6Ii8xMDIzMDIzMzgvNDExNjgwNjQ2LTA4YTg0NWVkLTI5ZmUtNGJjOS1hNzcwLTJiODA4MjcyOThjYy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjEwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxMFQxNzU2NDRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yOGI5YTVkMzY0OTE4ZDBiOWFjYjY2MDY3NDc2YzY2NWNjZTc5MjBhNjQxNWFjZTcwYmIzYzM0ODY2YWY2N2I0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.n3YSEwHw-acphlX8a7fkpVVVpUVFyo82MVYDR83-jG0",
+    link: "https://github.com/Hiuzton/E-Courses",
   },
 ];
 
-export const skills = ["film developing"];
+export const skills = ["C#", ".NET Framework 4.5", ".NET 8.0", "ASP.NET Core", "Entity Framework", "AWS S3, Amplify, EC2", "SOLID Principles", "Design Patterns"];
+
+export const languages = ["English", "Romanian", "Russian", "Ucranian"];
